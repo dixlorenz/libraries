725a4a12bb1caa01297c5bc406dff52edb483756
Dix Lorenz <dlorenz@medimach.de>
2018-07-09 11:36:47 +0200

    remove old DLCL::GCD::dispatch_on_main_queue

--- stlab/concurrency/main_executor.hpp
+++ stlab/concurrency/main_executor.hpp
@@ -123,7 +123,7 @@ struct main_executor_type {
     using result_type = void;
 
     template <typename F>
-    void operator()(F f) {
+    void operator()(F f) const {
         using f_t = decltype(f);
 
         dispatch_async_f(dispatch_get_main_queue(), new f_t(std::move(f)), [](void* f_) {
